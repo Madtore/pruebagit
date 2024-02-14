5a98d6a56508a464c671bae6989a7bfb97ab069b
Madtore <143751703+Madtore@users.noreply.github.com>
2024-02-14 16:30:34 +0100

    Segundo commit máster

--- pruebaegit/src/pruebaegit/PruebaEgit.java
+++ pruebaegit/src/pruebaegit/PruebaEgit.java
@@ -3,6 +3,7 @@ package pruebaegit;
 public class PruebaEgit {
 	public static void main(String[] args) {
 		System.out.println("Prueba Egit");
+		System.out.println("Segundo comit máster");
 	}
 
 }
